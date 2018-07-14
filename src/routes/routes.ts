15be0eadabae5b342230d2a1d05edb6493fa27da
surbhi mahajan <surbhi.mahajan@wingify.com>
2018-07-15 00:52:42 +0530

    feat(routes): fix added routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -13,6 +13,16 @@ export const routes = [
         name: 'about',
         component: () => import('../components/Layout/About/About.vue'),
       },
+      {
+          path: '/create',
+          name: 'create',
+          component: () => import('../components/Layout/Create/Create.vue'),
+      },
+      {
+          path: '/events',
+          name: 'events',
+          component: () => import('../components/Layout/Events/Events.vue'),
+      },
     ],
   },
   {
