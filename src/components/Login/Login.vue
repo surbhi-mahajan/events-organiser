48e8d1af02511ba68cd17397df2882032b5e0ed2
Punit Gupta <punit.gupta@wingify.com>
2018-08-08 23:15:55 +0530

    kuch pixels upr neeche :P

--- src/components/Login/Login.vue
+++ src/components/Login/Login.vue
@@ -10,10 +10,10 @@
                 <v-toolbar-title>Login</v-toolbar-title>
               </v-toolbar>
               <v-card-text>
-                <h2 style="display: flex; justify-content: center;">
-                  <img src="logo.jpg" width="30px" height="40px"/>                  
-                  <span style="padding: 5px 0px 5px 12px;">Minents</span>
-                </h2>
+                <div style="display: flex; justify-content: center;">
+                  <img src="logo_without_name.jpg" width="35px" height="40px"/>                  
+                  <span style="padding-left: 10px; font-size: 30px;">Minents</span>
+                </div>
               </v-card-text>
               <!-- <v-card-text>
                 <v-form>
@@ -30,7 +30,7 @@
                 :params="googleSignInParams"
                 @success="onGoogleSignInSuccess"
                 @error="onGoogleSignInError">
-                <div style="padding: 20px 0px 50px 0px;">
+                <div style="padding: 10px 0px 50px 0px;">
                   <div style="height: 36px; width: 170px; margin: auto;" class="abcRioButton abcRioButtonLightBlue">
                     <div class="abcRioButtonContentWrapper">
                       <div class="abcRioButtonIcon" style="padding: 8px">
